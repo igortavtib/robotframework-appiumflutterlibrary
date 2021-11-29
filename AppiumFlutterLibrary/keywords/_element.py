e9acad1aa083357867b20f1e5fa94a8a5eef646c
igortavtib <igortavtib@gmail.com>
2021-11-29 09:08:19 -0300

    feat: documentation

--- AppiumFlutterLibrary/keywords/_element.py
+++ AppiumFlutterLibrary/keywords/_element.py
@@ -10,6 +10,11 @@ class _ElementKeywords(KeywordGroup):
         self._element_finder = ElementFinder()
 
     def input_text(self, locator, text):
+        """Input text into the specified element.
+
+        If the element does not support text input an error
+        will be raised.
+        """
         element = self._find_element(locator)
         self._info("Typing text '%s' into component '%s'" % (text, locator))
         try:
@@ -18,6 +23,11 @@ class _ElementKeywords(KeywordGroup):
             raise err
 
     def clear_text(self, locator):
+        """Clear the text from specified element.
+
+        If the element does not support text clearing an error
+        will be raised.
+        """
         element = self._find_element(locator)
         self._info("Clearing text from element '%s'" % (locator))
         try:
@@ -26,6 +36,11 @@ class _ElementKeywords(KeywordGroup):
             raise err
 
     def click_element(self, locator):
+        """Tap the specified element.
+
+        If the element does not support tapping an error
+        will be raised.
+        """
         element = self._find_element(locator)
         self._info("Clicking on element %s" % locator)
         try:
@@ -34,17 +49,29 @@ class _ElementKeywords(KeywordGroup):
             raise err
 
     def element_should_be_visible(self, locator):
+        """Verify if the element is visible using FlutterDriver.waitFor().
+
+        If the element isn't visible raise an Assertion Error.
+        """
         element = self._find_element(locator)
         if not self._is_visible(element):
             raise AssertionError("Element '%s' should be visible but not" % locator)
 
     def element_text_should_be(self, locator, text):
+        """Verify if the element text is equal to provided text.
+
+        If the text isn't equal raise an Assertion Error.
+
+        If the element does not support getText() raise an error.
+        """
         element = self._find_element(locator)
         if element.text != text:
             raise AssertionError("Element '%s' text should be '%s' but is '%s'." %
                                     (locator, text, element.text))
 
     def get_element(self, locator):
+        """Returns the element object.
+        """
         return self._find_element(locator)
 
     def get_element_text(self, locator):
