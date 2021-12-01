ec8f0b0e59a92c684be1d5dbc61c8f6cdf7f621d
igortavtib <igortavtib@gmail.com>
2021-12-01 10:34:02 -0300

    feat: new keywords and locators

--- AppiumFlutterLibrary/keywords/_waiting.py
+++ AppiumFlutterLibrary/keywords/_waiting.py
@@ -24,6 +24,26 @@ class _WaintingKeywords(KeywordGroup):
                 application.execute_script('flutter:waitFor', element, timeout)
         except Exception:
             raise AssertionError("Could not find element '%s' in %s seconds" % (locator, timeout))
+    
+    def wait_for_element_absent(self, locator, timeout=20):
+        """Wait until element is absent. Uses FlutterDriver.waitForAbsent() to wait
+        for an element be absent for the specified timeout duration.
+
+        By default the timeout is 20 seconds.
+        """
+        application = self._current_application()
+        element = self._element_finder.find(application, locator)
+        if timeout == 0:
+            timeout=None
+        try:
+            if timeout is None:
+                application.execute_script('flutter:waitForAbsent', element)
+            else:
+                application.execute_script('flutter:waitForAbsent', element, timeout)
+        except Exception:
+            raise AssertionError("Element '%s' still not absent in %s seconds" % (locator, timeout))
+    
+
 
 
     def _format_timeout(self, timeout):
