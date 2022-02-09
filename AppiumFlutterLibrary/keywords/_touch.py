c801c4f12961667e34f6b2c119dd6318d29ee7a7
Guillaume Delaval <guillaume.delaval@orange.com>
2022-02-09 14:23:37 +0100

    feat: flutter scroll keyword

--- AppiumFlutterLibrary/keywords/_touch.py
+++ AppiumFlutterLibrary/keywords/_touch.py
@@ -27,3 +27,25 @@ class _TouchKeywords(KeywordGroup):
         driver = self._current_application()
         element = self._element_find(locator, True, True)
         driver.execute_script("mobile: scroll", {"direction": 'up', 'elementid': element.id})
+
+    def flutter_scroll(self, locator, dx, dy, durationmilliseconds=200, frequency=60):
+        """ Tell the driver to perform a scrolling action.
+        A scrolling action begins with a "pointer down" event, which commonly maps to finger press on the touch screen
+        or mouse button press. A series of "pointer move" events follow.
+        The action is completed by a "pointer up" event.
+
+        dx and dy specify the total offset for the entire scrolling action.
+
+        durationmilliseconds specifies the length of the action in milliseconds.
+
+        The move events are generated at a given frequency in Hz (or events per second). It defaults to 60Hz.
+        """
+        application = self._current_application()
+        element = self._element_finder.find(application, locator)
+        try:
+            application.execute_script('flutter:scroll',
+                                       element.id,
+                                       {'durationMilliseconds': int(durationmilliseconds), 'dx': int(dx), 'dy': int(dy),
+                                        'frequency': int(frequency)})
+        except Exception:
+            raise AssertionError("Unable to perform scroll")
